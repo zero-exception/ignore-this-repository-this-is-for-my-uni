b5d378409538cd6b026933d2fb2b1301706c21b1
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-13 20:13:52 +0400

    another task solved

---
+++ src/net/snezhniy/tasks/progs1/kotlin/Prog1.kt
@@ -0,0 +1,45 @@
+package net.snezhniy.tasks.progs1.kotlin
+
+import kotlin.math.exp
+import kotlin.math.sin
+
+/*
+Ввести начальное значение аргумента (xn), конечное значение аргумента
+(xk), шаг (dx) - все значения тип double. Протабулировать функцию
+y=exp(-x)*sin(x), т.е. для каждого значения аргумента из диапазона,
+рассчитать функцию. Значения аргумента и функции вывести на консоль.
+Программу написать с использованием цикла while.
+*/
+
+class Prog1 {
+    private fun isValidStep(xn: Double, xk: Double, dx: Double): Boolean {
+        val totalSteps = (xk - xn) / dx
+        return (totalSteps % 1.0) == 0.0
+    }
+
+    private fun calculateValues(x: Double): Double {
+        return exp(-x) * sin(x)
+    }
+
+    fun run() {
+        print("Укажите начальное значение аргумента (xn): ")
+        val xn = readLine()!!.toDouble()
+
+        print("Укажите конечное значение аргумента (xk): ")
+        val xk = readLine()!!.toDouble()
+
+        print("Укажите размер шага (dx): ")
+        val dx = readLine()!!.toDouble()
+
+        if (!isValidStep(xn, xk, dx)) {
+            return println("Вы указали некорректное количество шагов.")
+        }
+
+        var x = xn
+        while (x <= xk) {
+            val y = calculateValues(x)
+            println("x = $x, y(x) = $y")
+            x += dx
+        }
+    }
+}
