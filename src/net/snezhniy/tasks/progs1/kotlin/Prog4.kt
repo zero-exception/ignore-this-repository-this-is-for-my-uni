3827d3e3569ec6415d17145b287bcae77c12353d
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-14 17:51:31 +0400

    and one more

---
+++ src/net/snezhniy/tasks/progs1/kotlin/Prog4.kt
@@ -0,0 +1,21 @@
+package net.snezhniy.tasks.progs1.kotlin
+
+/*
+Создать и проинициализировать массив из 10 целых чисел. Рассчитать
+сумму элементов массива и их произведение и вывести результаты на
+консоль.
+*/
+
+class Prog4 {
+    fun run() {
+        // Иницилизация массива из 10 случайных чисел
+        val arr = Array(10) { (-1000..1000).random().toBigInteger() }
+        println("Элементы массива: ${arr.joinToString()}")
+
+        val sum = arr.reduce { acc, i -> acc + i }
+        println("Сумма элементов: $sum")
+
+        val prod = arr.reduce { acc, i -> acc * i }
+        println("Произведение элементов: $prod")
+    }
+}
