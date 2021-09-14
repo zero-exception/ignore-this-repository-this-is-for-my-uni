0c5835a033beaf0f726efcaa779362504c51d612
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-14 18:27:02 +0400

    YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET

---
+++ src/net/snezhniy/tasks/progs1/kotlin/Prog7.kt
@@ -0,0 +1,25 @@
+package net.snezhniy.tasks.progs1.kotlin
+
+/*
+Заполнить массив из 10 элементов случайными целыми числами.
+Подсчитать количество положительных чисел, количество отрицательных
+чисел и количество чисел из диапазона -5…..+5.
+*/
+
+class Prog7 {
+    fun run() {
+        val arr = Array(10) { (-5..5).random() }
+        println("Элементы массива: ${arr.joinToString()}")
+
+        val positives = arr.count { it > 0 }
+        println("Количество положительных: $positives")
+
+        val negatives = arr.count { it < 0 }
+        println("Количество отрицательных: $negatives")
+
+        // Диапазон может содержать нули, но при этом нули
+        // не являются положительными или отрицательными
+        val zeros = arr.count { it == 0 }
+        println("Количество нулей: $zeros")
+    }
+}
