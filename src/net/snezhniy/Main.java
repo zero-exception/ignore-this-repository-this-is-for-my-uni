703e5e2f3223c1b4efe46e2312e0d145733bc6a1
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-09-21 12:02:56 +0400

    get rid of bad code

--- src/net/snezhniy/Main.java
+++ src/net/snezhniy/Main.java
@@ -1,11 +1,34 @@
 package net.snezhniy;
 
-import net.snezhniy.kotlin.tasks.conditions.*;
+import net.snezhniy.java.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        var task = new Conditions2();
+        var arr = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 3; i++) {
+            var rnd = new Random();
+            var innerList = new ArrayList<Integer>();
+            for (int j = 0; j < 3; j++) {
+                var n = rnd.nextInt(65536) - 32768;
+                innerList.add(j, n);
+            }
+            arr.add(innerList);
+        }
 
-        task.run();
+        var joined = Utils.joinTwoDimArray(arr);
+        System.out.printf("%s\n", joined);
+
+        var col = Utils.getColumn(arr, 1);
+        var joined2 = col
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+
+        System.out.printf("%s", joined2);
     }
 }
